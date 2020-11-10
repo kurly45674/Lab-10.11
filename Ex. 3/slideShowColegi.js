5895d96a0844d90e59718ebc7d81d6a677359095
kurly45674 <73156180+kurly45674@users.noreply.github.com>
2020-11-10 23:12:01 +0200

    Add files via upload

---
+++ Ex. 3/slideShowColegi.js
@@ -0,0 +1,16 @@
+var slideIndex=0;
+showSlidesColegi();
+
+function showSlidesColegi()
+	{
+	var i;
+	var slides = document.getElementsByClassName("myslides");
+	for (i=0; i<slides.length; i++){
+		slides[i].style.display = "none";
+	}
+	slideIndex++;
+	if (slideIndex>slides.length)
+		{slideIndex=1;}
+	slides[slideIndex-1].style.display = "block";
+	setTimeout(showSlidesColegi, 3000);
+}
